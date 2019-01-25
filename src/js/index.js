e2cebd22780cc582b8d7bc3e15c72c2fdf2ea5bc
Daniel Slowik <kkasztann@gmail.com>
2019-01-25 11:27:49 +0100

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 /* Here goes your JS code */
+
+window.onload = function () {
+    var clickMeBtn = document.getElementById('clickMeBtn');
+    clickMeBtn.addEventListener('click', function (event) {
+        location.href = "#show-popup-form";
+    });
+};
